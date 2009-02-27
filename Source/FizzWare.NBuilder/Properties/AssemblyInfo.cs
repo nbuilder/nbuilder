5e5a3243e0bb28eb8f04e8009eaeeb8ac55c1bda
garethdown44 <garethdown44@8cbb6a18-f00b-11dd-ada4-5153b8187bf2>
2009-02-27 22:42:27 +0000

    git-svn-id: http://nbuilder.googlecode.com/svn/trunk/@6 8cbb6a18-f00b-11dd-ada4-5153b8187bf2

--- Source/FizzWare.NBuilder/Properties/AssemblyInfo.cs
+++ Source/FizzWare.NBuilder/Properties/AssemblyInfo.cs
@@ -7,10 +7,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("FizzWare.NBuilder")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Diamond Trading Company")]
+[assembly: AssemblyCompany("FizzWare")]
 [assembly: AssemblyProduct("FizzWare.NBuilder")]
-[assembly: AssemblyCopyright("Copyright © Diamond Trading Company 2009")]
+[assembly: AssemblyCopyright("Copyright © FizzWare 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1dae098c-5bb4-437c-9f77-2d702c472960")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
