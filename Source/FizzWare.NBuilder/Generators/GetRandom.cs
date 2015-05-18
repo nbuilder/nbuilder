fd957fc05be7eac0a56089361ad396ce60ed2150
Gareth Down <g@garethdown.com>
2015-05-18 07:23:31 +0100

    Revert "make GetRandom use a thread-safe generator"

--- Source/FizzWare.NBuilder/Generators/GetRandom.cs
+++ Source/FizzWare.NBuilder/Generators/GetRandom.cs
@@ -6,21 +6,7 @@ namespace FizzWare.NBuilder.Generators
 {
     public class GetRandom
     {
-        [ThreadStatic]
-        private static IRandomGenerator __threadSafeGenerator;
-
-        private static IRandomGenerator generator
-        {
-            get
-            {
-                if (__threadSafeGenerator == null)
-                {
-                    __threadSafeGenerator = new RandomGenerator();
-                }
-
-                return __threadSafeGenerator;
-            }
-        }
+        private static readonly IRandomGenerator generator = new RandomGenerator();        
         
         private static DateTime minSqlServerDate = new DateTime(1753, 1, 1);
         private static DateTime maxSqlServerDate = new DateTime(9999, 12, 31);
