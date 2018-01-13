ac26f6e529f2c30257abfba0dea667bd173257b0
Rik Svendsen Rose <rsr@gait.dk>
2018-01-13 10:50:48 +0100

    #73 Add test that tuple should not be null

---
+++ tests/FizzWare.NBuilder.Tests/Integration/TupleTests.cs
@@ -0,0 +1,22 @@
+﻿using FizzWare.NBuilder.PropertyNaming;
+using FizzWare.NBuilder.Tests.TestClasses;
+using NSubstitute;
+
+using Shouldly;
+using Xunit;
+
+
+namespace FizzWare.NBuilder.Tests.Integration
+{
+    public class TupleTests
+    {
+        [Fact]
+        public void _Build_Tuple_DoesNotReturnNullValues()
+        {
+            var target = new Builder().CreateNew<(MyClass Class1, MyClass Class2)>();
+            var actual = target.Build();
+            actual.Class1.ShouldNotBeNull();
+            actual.Class2.ShouldNotBeNull();
+        }
+    }
+}
