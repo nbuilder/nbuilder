40cdd5da3612c2a88617207fdae7f30ff4a0b1ce
Doug Murphy <doug.murphy82@gmail.com>
2019-11-21 20:35:29 -0500

    Fulfills #98 by adding in new methods to GetRandom static class for IPv6 and MAC addresses.

---
+++ tests/FizzWare.NBuilder.Tests/Unit/GetRandomTests.cs
@@ -0,0 +1,70 @@
+﻿using FizzWare.NBuilder.Generators;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace FizzWare.NBuilder.Tests.Unit
+{
+    public class GetRandomTests
+    {
+        [Fact]
+        public void GetRandom_GeneratesCorrectIPv4Address()
+        {
+            var expectedRegex = new Regex("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", RegexOptions.IgnoreCase);
+            // Arrange
+            for (int i = 0; i < 100; i++)
+            {
+                // Act
+                var result = GetRandom.IpAddress();
+
+                // Assert
+                Assert.Matches(expectedRegex, result);
+            }
+        }
+
+        [Fact]
+        public void GetRandom_GeneratesCorrectIPv6Address()
+        {
+            var expectedRegex = new Regex("^[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}$", RegexOptions.IgnoreCase);
+            // Arrange
+            for (int i = 0; i < 100; i++)
+            {
+                // Act
+                var result = GetRandom.IpAddressV6();
+
+                // Assert
+                Assert.Matches(expectedRegex, result);
+            }
+        }
+
+        [Fact]
+        public void GetRandom_GeneratesCorrectMacAddress()
+        {
+            var expectedRegex = new Regex("^[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}$", RegexOptions.IgnoreCase);
+            // Arrange
+            for (int i = 0; i < 100; i++)
+            {
+                // Act
+                var result = GetRandom.MacAddress();
+
+                // Assert
+                Assert.Matches(expectedRegex, result);
+            }
+        }
+
+        [Fact]
+        public void GetRandom_OverrideSeparator_GeneratesCorrectMacAddress()
+        {
+            var overriddenSeparator = ":";
+            var expectedRegex = new Regex($"^[0-9a-f]{{2}}{overriddenSeparator}[0-9a-f]{{2}}{overriddenSeparator}[0-9a-f]{{2}}{overriddenSeparator}[0-9a-f]{{2}}{overriddenSeparator}[0-9a-f]{{2}}{overriddenSeparator}[0-9a-f]{{2}}$", RegexOptions.IgnoreCase);
+            // Arrange
+            for (int i = 0; i < 100; i++)
+            {
+                // Act
+                var result = GetRandom.MacAddress(overriddenSeparator);
+
+                // Assert
+                Assert.Matches(expectedRegex, result);
+            }
+        }
+    }
+}
