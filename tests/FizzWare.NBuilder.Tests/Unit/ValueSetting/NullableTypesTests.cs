1cc800b1c7b5102264e9457fdcdf769b61eb4504
Pure Krome <github@world-domination.com.au>
2025-12-26 11:24:48 +1100

    Copilot feedback

--- tests/FizzWare.NBuilder.Tests/Unit/ValueSetting/NullableTypesTests.cs
+++ tests/FizzWare.NBuilder.Tests/Unit/ValueSetting/NullableTypesTests.cs
@@ -1,71 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using FizzWare.NBuilder.Implementation;
-using FizzWare.NBuilder.PropertyNaming;
-using FizzWare.NBuilder.Tests.TestClasses;
-
-using NSubstitute;
+﻿using FizzWare.NBuilder.Tests.TestClasses;
 using Shouldly;
+using System;
 using Xunit;
 
 
-namespace FizzWare.NBuilder.Tests.Unit
+namespace FizzWare.NBuilder.Tests.Unit.ValueSetting;
+
+
+public class NullableTypesTests
 {
+    readonly BuilderSettings settings;
+    readonly Builder builder;
 
-    public class NullableTypesTests
+    public NullableTypesTests()
     {
-        BuilderSettings settings;
-        Builder builder;
-
-        public NullableTypesTests()
-        {
-            this.settings = new BuilderSettings();
-            this.builder = new Builder(this.settings);
-        }
-
-        [Fact]
-        public void WhenAllNullableTypesShouldBeNullByDefault()
-        {
-            this.settings.UseNullAsDefaultValueForAllNullableTypes();
-            var value = builder.CreateNew<MyClass>().Build();
-            value.NullableInt.HasValue.ShouldBeFalse();
-            value.NullableGuid.HasValue.ShouldBeFalse();
-        }
-
-        [Fact]
-        public void ValueIsDefaultForType_Int()
-        {
-            var value = builder.CreateNew<MyClass>().Build();
-            value.NullableInt.HasValue.ShouldBeTrue();
-            value.NullableInt.ShouldBe(1);
-        }
-
-        [Fact]
-        public void UseNullAsDefaultValueForNullableType_Int_ValueIsNull()
-        {
-            this.settings.UseNullAsDefaultValueForNullableType(typeof(int?));
-            var value = builder.CreateNew<MyClass>().Build();
-            value.NullableInt.HasValue.ShouldBeFalse();
-        }
-
-
-        [Fact]
-        public void ValueIsDefaultForType_Guid()
-        {
-            var value = builder.CreateNew<MyClass>().Build();
-            value.NullableGuid.HasValue.ShouldBeTrue();
-            value.NullableGuid.ShouldNotBe(Guid.Empty);
-        }
-
-        [Fact]
-        public void UseNullAsDefaultValueForNullableType_Guid_ValueIsNull()
-        {
-            this.settings.UseNullAsDefaultValueForNullableType(typeof(Guid?));
-            var value = builder.CreateNew<MyClass>().Build();
-            value.NullableGuid.HasValue.ShouldBeFalse();
-        }
-
+        this.settings = new BuilderSettings();
+        this.builder = new Builder(this.settings);
     }
 
+    [Fact]
+    public void WhenAllNullableTypesShouldBeNullByDefault()
+    {
+        this.settings.UseNullAsDefaultValueForAllNullableTypes();
+        var value = builder.CreateNew<MyClass>().Build();
+        value.NullableInt.HasValue.ShouldBeFalse();
+        value.NullableGuid.HasValue.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void ValueIsDefaultForType_Int()
+    {
+        var value = builder.CreateNew<MyClass>().Build();
+        value.NullableInt.HasValue.ShouldBeTrue();
+        value.NullableInt.ShouldBe(1);
+    }
+
+    [Fact]
+    public void UseNullAsDefaultValueForNullableType_Int_ValueIsNull()
+    {
+        this.settings.UseNullAsDefaultValueForNullableType(typeof(int?));
+        var value = builder.CreateNew<MyClass>().Build();
+        value.NullableInt.HasValue.ShouldBeFalse();
+    }
+
+
+    [Fact]
+    public void ValueIsDefaultForType_Guid()
+    {
+        var value = builder.CreateNew<MyClass>().Build();
+        value.NullableGuid.HasValue.ShouldBeTrue();
+        value.NullableGuid.ShouldNotBe(Guid.Empty);
+    }
+
+    [Fact]
+    public void UseNullAsDefaultValueForNullableType_Guid_ValueIsNull()
+    {
+        this.settings.UseNullAsDefaultValueForNullableType(typeof(Guid?));
+        var value = builder.CreateNew<MyClass>().Build();
+        value.NullableGuid.HasValue.ShouldBeFalse();
+    }
 
 }
